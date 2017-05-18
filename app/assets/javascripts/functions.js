0fbb93ae149cf384e7b0f3c204e75efcb746987f
Xunze Su <xunzesu@Xunzes-MacBook-Pro.local>
2017-05-18 01:58:13 -0400

    Finish filter

--- app/assets/javascripts/functions.js
+++ app/assets/javascripts/functions.js
@@ -6,12 +6,12 @@ $(document).on('page:restore', function(){
   Paloma.start();
 });
 
-var initializeMap = function(centroidLatLngArray){
-  return L.map('index-map', {
+var initializeMap = function(mapId, zoonLevel, centroidLatLngArray, isZoomable, isZoomControl){
+  return L.map(mapId, {
     center: centroidLatLngArray,
-    zoom: 8,
-    scrollWheelZoom: false,
-    zoomControl:false
+    zoom: zoonLevel,
+    scrollWheelZoom: isZoomable,
+    zoomControl: isZoomControl
   });
 };
 
@@ -51,6 +51,121 @@ var countyStyle = function(feature){
   };
 };
 
+var lineWidth = function(breaks, crashCount){
+  return crashCount >= breaks[2][0]? 8 :
+         crashCount >= breaks[1][0]? 5 :
+                                     2;
+
+  // console.log(breaks, crashCount)
+  // if(crashCount >= breaks[2][0]){ return 10; }
+  // else if(crashCount >= breaks[1][0]){ return 1; }
+  // else { return 1; }
+};
+
+var lineColor = function(breaks, crashCount){
+  return crashCount >= breaks[2][0]? "#3844b3" :
+         crashCount >= breaks[1][0]? "#7881d5" :
+                                     "#c3c7ec";
+};
+
+var weatherCondition = function(year, weatherCode){
+  switch(weatherCode) {
+    case 0:
+      return year >= 2010 ? "No Additional Atmospheric Conditions" : "No Adverse Atmospheric Conditions";
+      break;
+    case 1:
+      return year >= 2010 ? "Clear" : "No Adverse Atmospheric Conditions";
+      break;
+    case 2:
+      return "Rain";
+      break;
+    case 3:
+      return "Sleet, Hail";
+      break;
+    case 4:
+      return "Snow";
+      break;
+    case 5:
+      return "Fog";
+      break;
+    case 6:
+      return year >= 2007 ? "Severe Crosswinds" : "Rain and Fog";
+      break;
+    case 7:
+      return year >= 2007 ? "Blowing Sand, Soil, Dirt" : "Sleet and Fog";
+      break;
+    case 8:
+      return "Other";
+      break;
+    case 9:
+      return "Unknown";
+      break;
+    case 10:
+      return "Cloudy";
+      break;
+    case 11:
+      return "Blowing Snow";
+      break;
+    case 12:
+      return "Freezing Rain or Drizzle";
+      break;
+    case 98:
+      return "Not Reported";
+      break;
+    case 99:
+      return "Unknown";
+      break;
+    default:
+      return "";
+  }
+}
+
+//all have popups with crash info
+//when hovered, the circle becomes bigger
+var addCrashPoints = function(year, eachCrashObject, map){
+  var popupHTML = "<p><b>Date</b>: " + eachCrashObject.month_text + " " + eachCrashObject.day + "</p>"
+                + "<p><b>Day</b>: " + eachCrashObject.day_week_text + "</p>"
+                + "<p><b>Time</b>: ~" + eachCrashObject.hour + ":00</p>"
+                + "<p><b>Fatalities</b>: " + eachCrashObject.fatals + "</p>"
+                + "<p><b>Drunk Individuals</b>: " + eachCrashObject.drunk + "</p>"
+                + "<p><b>Weather</b>: " + weatherCondition(year, eachCrashObject.weather) + "</p>";
+
+  return L.circleMarker([eachCrashObject.latitude, eachCrashObject.longitude],{
+    radius: 5,
+    stroke: false,
+    fillOpacity: 0.8,
+    fillColor: '#b33844',
+  })
+  .bindPopup(popupHTML)
+  .on({
+    mouseover: function(e){ e.target.setRadius(10); },
+    mouseout: function(e) { e.target.setRadius(5);  }
+  })
+  .addTo(map);
+}
+
+var mapIdToKey = function(id){
+  switch(id) {
+    case "month":
+      return "month_text";
+      break;
+    case "day":
+      return "day_week_text";
+      break;
+    default:
+      return id;
+  }
+}
+
+var pluckCrashInfoValues = function(dataCrash, key){
+  return _.chain(dataCrash)
+          .pluck(key)
+          .uniq()
+          .sortBy(function(num){
+            return num
+          }).
+          value();
+}
 
 Paloma.controller('Crashes', {
 
@@ -65,7 +180,7 @@ Paloma.controller('Crashes', {
     var countyGeojson = this.params.county;
 
     //Initialize the map and make the center in PA
-    var mapIndex = initializeMap([40.967418, -77.106913]);
+    var mapIndex = initializeMap('index-map', 8, [40.967418, -77.106913], false, false);
 
     //Crash counts in all counties from 2004 to 2013
     var allCrashCounts = objectValues(_.pluck(countyGeojson.features,'properties'),
@@ -151,15 +266,147 @@ Paloma.controller('Crashes', {
 
   search: function(){
 
-    var crashes = this.params.crashes,
-        roads = this.params.roads,
-        year = this.params.year,
-        fips = this.params.fips;
+    //variables sent from the back end controller
+    var crashes = this.params.crashes, //an array of crash point objects from db query based on county fips and year
+        roads = this.params.roads, //geojson from github based on county fips
+        year = this.params.year, //year of this search
+        fips = this.params.fips, //county fips of this search
+        countyCentroid = this.params.county_centroid, //[lat, lng] of the county centroid, from github county centorid data
+        countyName = this.params.county_name; //county name
 
-    console.log(crashes);
-    console.log(roads);
-    console.log(year);
-    console.log(fips);
+    //hide all attribute filter dropdown
+    $(".filter-dropdown").hide();
+    //populate the title with county name and year
+    $("#search-header-county").text(countyName);
+    $("#search-header-year").text(year);
+
+    //all crash counts on road segments
+    var allRoadCrashCounts = objectValues(_.pluck(roads.features,'properties'), ["crash_" + year]);
+    //three buckest of values to break the crash counts on roads into three parts
+    var allRoadCrashCountsBreaks = _.map(ss.ckmeans(allRoadCrashCounts, 3), function(group){ return [_.first(group), _.last(group)]});
+
+    //initialize this county's map
+    var mapSearch = initializeMap('search-map', 12, countyCentroid, true, true);
+    var cartoToner = L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png', {
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      minZoom: 0,
+      maxZoom: 20,
+      ext: 'png'
+    }).addTo(mapSearch);
+
+    //add the roads to this map, crash counts on roads are represented by width and color of lines
+    var layerRoads = L.geoJSON(roads, {
+      style: function(feature){
+        return {
+          weight: lineWidth(allRoadCrashCountsBreaks, feature.properties["crash_" + year]),
+          color: lineColor(allRoadCrashCountsBreaks, feature.properties["crash_" + year])
+        }
+      }
+    })
+    .addTo(mapSearch);
+
+    //add all crash points to the map
+    var layerCrashPoints = _.map(crashes, function(eachCrashObject){
+      return addCrashPoints(year, eachCrashObject, mapSearch)
+    });
+
+    //Append options to the dropdown based on this queried data
+    var months = pluckCrashInfoValues(crashes, "month");
+    _.each(months, function(monthNum){
+      var monthText = _.find(crashes, function(crash){return crash.month == monthNum}).month_text;
+      $("#dropdown-month").append("<li class='mdl-menu__item' id='month-"+ monthText +"'>" + monthText + "</li>");
+    });
+    var days = pluckCrashInfoValues(crashes, "day_week");
+    _.each(days, function(dayNum){
+      var dayText = _.find(crashes, function(crash){return crash.day_week == dayNum}).day_week_text;
+      $("#dropdown-day").append("<li class='mdl-menu__item' id='day-"+ dayText +"'>" + dayText + "</li>");
+    });
+    var hours = pluckCrashInfoValues(crashes, "hour");
+    _.each(hours, function(hourNum){
+      var hourText = (hourNum == 88 || hourNum == 99 ? "Unknown" : "~ " + hourNum + ":00");
+      $("#dropdown-hour").append("<li class='mdl-menu__item' id='hour-"+ hourNum +"'>" + hourText + "</li>");
+    });
+    var fatals = pluckCrashInfoValues(crashes, "fatals");
+    _.each(fatals, function(fatalNum){
+      $("#dropdown-fatals").append("<li class='mdl-menu__item' id='fatals-"+ fatalNum +"'>" + fatalNum + "</li>");
+    });
+    var drunks = pluckCrashInfoValues(crashes, "drunk");
+    _.each(drunks, function(drunkNum){
+      $("#dropdown-drunk").append("<li class='mdl-menu__item' id='drunk-"+ drunkNum +"'>" + drunkNum + "</li>");
+    });
+    var weathers = pluckCrashInfoValues(crashes, "weather");
+    _.each(weathers, function(weatherNum){
+      var weatherText = weatherCondition(year, weatherNum);
+      $("#dropdown-weather").append("<li class='mdl-menu__item' id='weather-"+ weatherNum +"'>" + weatherText + "</li>");
+    });
+
+
+    //when one of three tabs is clicked
+    //clear the current displayed crash points (all or filtered)
+    //add all crash points to the map
+    $(".mdl-tabs__tab").on("click", function(e){
+      if(layerCrashPoints.length){
+        _.each(layerCrashPoints, function(eachLayer){ mapSearch.removeLayer(eachLayer);})
+        layerCrashPoints = [];
+      }
+      layerCrashPoints = _.map(crashes, function(eachCrashObject){
+        return addCrashPoints(year, eachCrashObject, mapSearch)
+      });
+    });
+
+    //when the dropdown option is clicked, fill in the text field
+    //clear all points on the map
+    //filter data based on selected option
+    //add the filtered data to the map
+    $(".mdl-menu__item").on("click", function(e){
+      //this id is in the form of xx-yy, e.g.: month-Januaray
+      var id = this.id;
+      var htmlId = id.split("-")[0];
+      var value = id.split("-")[1];
+
+      //fill in the text field for the dropdown
+      var fillVal;
+      if(htmlId == "hour"){
+        fillVal = (value == 88 || value == 99 ? "Unknown" : "~ " + value + ":00");
+      }else if(htmlId == "weather"){
+        fillVal = weatherCondition(year, value);
+      }else {
+        fillVal = value;
+      }
+      $("#filter-" + id.split("-")[0] + "-dropdown-selected").val(fillVal);
+
+      //clear all points on the map and the variable storing the points
+      if(layerCrashPoints.length){
+        _.each(layerCrashPoints, function(eachLayer){ mapSearch.removeLayer(eachLayer);})
+        layerCrashPoints = [];
+      }
+
+      //add the filtered points to the map
+      layerCrashPoints = _.chain(crashes)
+                          .filter(function(crash){ return crash[mapIdToKey(id.split("-")[0])].toString() == id.split("-")[1]; })
+                          .map(function(point){ return addCrashPoints(year, point, mapSearch); })
+                          .value();
+    });
+
+    //when any radio button is clicked, hide all dropdowns first
+    //and then show the corresponding dropdown
+    //clear any dropdown text field
+    $('input[id*="radio-"]').on("click", function(e){
+      var id = this.id.split("-")[1];
+      console.log(id)
+      $(".filter-dropdown").hide();
+      $("#filter-" + id + "-dropdown").show();
+      $("input[id*='-dropdown-selected']").val("");
+    });
+
+
+    console.log("Crashes:", crashes);
+    console.log("Roads:", roads);
+    console.log("Year:", year);
+    console.log("FIPS:", fips);
+    console.log("County Centroids:", countyCentroid);
+    console.log("County Names:", countyName);
 
   }
 })
--- app/controllers/crashes_controller.rb
+++ app/controllers/crashes_controller.rb
@@ -19,14 +19,29 @@ class CrashesController < ApplicationController
 
     @year = params[:year]
     @fips = params[:fips]
+    @county_centroids = Hash.new
 
     @crashes = Crash.where("year = ? AND fips = ?", @year.to_i, @fips.to_i)
 
     open("https://raw.githubusercontent.com/aaronxsu/MyData/master/PA_Road_Crash_Count_by_County_2004_2013_GeoJSON/" + @fips.to_s + ".geojson") do |f|
-     @roads = JSON.parse(f.read)
+      @roads = JSON.parse(f.read)
     end
 
-    js :crashes => @crashes, :roads => @roads, :year => @year, :fips => @fips
+    open("https://raw.githubusercontent.com/aaronxsu/MyData/master/pa_county_centroid.json") do |f|
+      @county_centroids = JSON.parse(f.read).select { |county| county["FIPS"] == @fips.to_i }.first
+    end
+
+    @keys = ["id", "name", "label"]
+    @id_values = ["month", "day", "hour", "fatals", "drunk", "weather"]
+    @id_labels = ["Month of a Year", "Day of a Week", "Time of a Day", "Number of Fatalities", "Drunk Individuals", "Weather Conditions"]
+
+    @attr_filter_html = Array.new
+    @id_values.each_with_index do |val, index|
+      @attr_filter_html.push([[@keys[0], val], [@keys[1], val.capitalize + "..."], [@keys[2], @id_labels[index]]].to_h)
+    end
+
+
+    js :crashes => @crashes, :roads => @roads, :year => @year, :fips => @fips, :county_name => @county_centroids['County'], :county_centroid => [@county_centroids["center_lat"], @county_centroids["center_lng"]]
 
   end
 
